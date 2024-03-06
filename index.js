8a396bf00bc73391991ee1fffc2964ce037f806d
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-03-06 11:34:37 -0300

    Corrigindo o arquivo index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const input = document.getElementById('input')
 import { calculate } from "./functions/calculate.js"
 import copyToClipBoard from "./functions/copyToClipBoard.js"
 import themeSwitcher from "./functions/themeSwitcher.js"
+import keydown from "./functions/keydown.js"
 
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
@@ -20,20 +21,7 @@ document.getElementById('clear').addEventListener('click', function(){
 
 document.getElementById('equal').addEventListener('click', calculate)
 
-input.addEventListener('keydown', function (ev){
-    ev.preventDefault()
-
-    if(allowedKeys.includes(ev.key)){
-        input.value += ev.key
-        return
-    }
-    if(ev.key === 'Backspace'){
-        input.value = input.value.slice(0, -1)
-    }
-    if(ev.key === "Enter"){
-        calculate()
-    }
-})
+input.addEventListener('keydown', keydown)
 
 document.getElementById("equal").addEventListener("click", calculate)
 
